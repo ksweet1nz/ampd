c2564aae1d5d9032027de18e9c0ad753096ae262
Kevin Sweet <kevin.sweet.nz@gmail.com>
2018-04-07 10:39:02 +1200

    social media icons

--- admin/templates/layouts/global.footer.php
+++ admin/templates/layouts/global.footer.php
@@ -1,4 +1,8 @@
 <footer>
+    <ul class="soc">
+      <li><a class="soc-instagram" href="https://www.instagram.com/ampdlabnz/"></a></li>
+      <li><a class="soc-facebook" href="https://www.facebook.com/AMPDLab"></a></li>
+    </ul>
     &copy; <?php echo date("Y"); ?> Advanced Manufacturing and Prototyping for Design
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
