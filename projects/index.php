b6cbfdb810456a93163c3a7ceceb0211ff0cedb3
Kevin <kevin.sweet.nz@gmail.com>
2018-05-05 19:00:54 +1200

    project list with anchors

--- projects/index.php
+++ projects/index.php
@@ -8,6 +8,17 @@
   <div class="container">
     <?php perch_layout('global.header'); ?>
     <main>
+      <article class="project-article">
+        <h1>2018 Projects</h1>
+        <ul class="project-list">
+          <?php
+            perch_content_custom('Projects', array(
+              'template'=>'project-list.html',
+              'page'=>'/projects/index.php'
+            ));
+          ?>
+        </ul>
+      </article>
       <?php perch_content('Projects'); ?>
     </main>
     <?php perch_layout('global.footer'); ?>
